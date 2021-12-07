f7c69e77d88cbecc7192fcb3f981d44e0a17456a
ochi <mune.ochi@gmail.com>
2021-12-08 00:25:34 +0900

    add Jetpack Compose. update help activity

--- app/src/main/java/com/example/localweatherapp/HelpActivity.kt
+++ app/src/main/java/com/example/localweatherapp/HelpActivity.kt
@@ -1,19 +1,45 @@
 package com.example.localweatherapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.material.composethemeadapter.MdcTheme
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help)
-
-        findViewById<TextView>(R.id.tvLicense).setOnClickListener {
-            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        setContent {
+            MdcTheme() {
+                License(this)
+            }
         }
-
     }
+}
+
+@Composable
+private fun License(context: Context) {
+    Text(
+        text = stringResource(id = R.string.oss_license_title),
+        style = MaterialTheme.typography.body1,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, top = 10.dp)
+            .clickable(
+                enabled = true
+            ){
+                context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+            },
+    )
 }
